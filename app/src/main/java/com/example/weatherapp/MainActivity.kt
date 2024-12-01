d7ac2be6b0ba6901e06b1f0ec9cc9dc44ea1e579
Fahad <fsadakathussain@gmail.com>
2024-12-01 23:08:59 +0600

    Screens added

--- app/src/main/java/com/example/weatherapp/MainActivity.kt
+++ app/src/main/java/com/example/weatherapp/MainActivity.kt
@@ -5,7 +5,12 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.example.weatherapp.nav.WeatherAppNavGraph
@@ -30,8 +35,11 @@ class MainActivity : ComponentActivity() {
                 LocalNavController provides navController, LocalMainViewModel provides viewModel
             ) {
                 WeatherAppTheme {
-                    WeatherAppNavGraph(navController)
-
+                    Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                        Box(modifier = Modifier.padding(innerPadding)) {
+                            WeatherAppNavGraph(navController)
+                        }
+                    }
                 }
             }
         }
--- app/src/main/java/com/example/weatherapp/nav/WeatherAppNavGraph.kt
+++ app/src/main/java/com/example/weatherapp/nav/WeatherAppNavGraph.kt
@@ -1,9 +1,13 @@
 package com.example.weatherapp.nav
 
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.BackHandler
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import com.example.weatherapp.ui.screens.HomeScreen
+import com.example.weatherapp.ui.screens.SearchScreen
 import com.example.weatherapp.utils.LocalNavController
 
 @Composable
@@ -14,10 +18,13 @@ fun WeatherAppNavGraph(
 
     NavHost(navController = navController, startDestination = startDestination) {
         composable(route = NavScreens.HOME_SCREEN) {
-
+            HomeScreen()
+            BackHandler {
+                (currentNav.context as ComponentActivity).finish()
+            }
         }
         composable(route = NavScreens.SEARCH_SCREEN) {
-
+            SearchScreen()
         }
     }
 }
